0f9815f10b8b9e3318b2d590063087914a565643
Amrakh <emrahamiraslanov@gmail.com>
2022-04-13 16:11:21 +0400

    third

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-// План действий:
-// 1) Внизу списка находится кнопка «Добавить». При нажатии на неё в конец списка добавляется новая пустая запись.
-// 2) Над списком присутствует кнопка сортировки. При нажатии на неё записи сортируются по алфавиту. 
-// 3) Повторное нажатие приводит к сортировке по алфавиту в обратном порядке. 
-// 4) После каждого нажатия кнопка меняет свое состояние, показывая пользователю, 
-// в каком порядке будет происходить сортировка (прямом или обратном).
-// 5) В начальном состоянии список содержит одну пустую запись.
-// 6) drag and drop.
+
 
 let array = [''];
 
@@ -21,7 +14,6 @@ renderList();
 addTaskButton.addEventListener('click', addHandler);
 
 sort.addEventListener('click', sortButtonChange);
-
 
 function renderList() {
     otherTasks.innerHTML = '';
@@ -116,115 +108,3 @@ function createTaskElement(arrayEl, index) {
     block.append(input, xButton);
     return block
 }
-
-
-
-
-
-// function xButtonHandler() {
-//     array = array.filter((item, index) => item != array[index]);
-//     block.remove()
-// };
-
-// let sortButtonCreate = (() => {
-//     sortingNew = document.createElement('div');
-//     sortingNew.classList.add('sorting');
-//     mainBlock.append(sortingNew);
-
-//     sortUpNew = document.createElement('button');
-//     sortUpNew.classList.add('sort-up');
-//     sorting.append(sortUpNew);
-// });
-
-// console.log(sortButtonCreate);
-// console.dir(sortButtonCreate);
-
-
-// function sortUpHandler () {
-//     otherTasks.innerHTML = '';
-
-//     data = array.sort((a,b) => b - a);
-
-//     data.forEach((item) => {
-//         otherTasks.append(createTaskElement(item));
-//     });
-// }
-
-
-// здесь мы запускаем forEach для каждого элемента массива array - и для каждого мы делаем:
-// tasker.append - подвязываем вниз элемента tasker (окошко с задачами) новый элемент.
-// (createTaskElement(item)) // это то, ЧТО мы подвязываем.
-// event.target.innerText = 'Нажала кнопку и меняю на ней текст';
-// event.target.style.backgroundColor = "aquamarine";
-// event.target.style.color = "black";
-
-
-// Пробовала как работает:
-// task.addEventListener('change', (event) => {
-//     console.log(event.target);
-//     console.log(event.target.value);
-// });
-
-
-// function clickHandler() {
-//     array.push(task.value);
-//     console.log(array);
-// }
-
-// Эта функция делает "отрисовку" таскера на странице при каждом запуске события - клика по кнопке "Добавить".
-
-
-
-// console.dir(addTaskButton);
-// Добавили обработчик события "клик" на кнопку "Добавить" (addTaskButton).
-// addTaskButton.addEventListener('click', clickHandler);
-
-
-
-// Что делает эта функция? эта функция и есть сам обработчик события, т.е. что происходит при клике на кнопку 
-// (что браузер делает при возникновении события).
-
-
-
-// data.forEach((item) => {
-    //     otherTasks.append(createTaskElement(item));
-    // });
-
-
-    // function xButtonHandler() {
-    //     console.log(array);
-    //     if (array.length > 1) {
-    //         array = array.filter(item => item != arrayEl);
-    //         block.remove();
-    //     } else if (array.length ===1) {
-    //         input.value = "";
-    //     }
-    // };
-
-
-
-
-    // if (array.length > 1) {
-    //     array = array.filter(item => item != arrayEl)
-    //     block.remove();
-    // } else if (array.length === 1) {
-    //     input.value = "";
-    // }
-
-
-    // array = array.filter((item,index) => {item[index] != input.value[input.id]});
-
-
-    // array = array.filter((item,index) => {
-    //     return item[index] != taskToDelete.value[taskToDelete.id];
-
-
-
-     // array = array.filter((item,index) => {
-        //     return item[index] != taskToDelete.value[taskToDelete.id];
-        //     // console.log(item, index)
-        //     // return item[index];
-        // });
-        // parent.remove();
-        // console.log(array);
-        // block.remove();
